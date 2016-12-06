c1ba740624e39ed90defaab7546dbd92e8c30723
Paul Zabelin <paulz@doordash.com>
2016-12-05 16:22:05 -0800

    use implicity requirements for Quick pod

--- Example/Pods/Target Support Files/Pods-KIF-Quick_Example/Pods-KIF-Quick_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-KIF-Quick_Example/Pods-KIF-Quick_Example-frameworks.sh
@@ -82,14 +82,3 @@ strip_invalid_archs() {
   fi
 }
 
-
-if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/KIF/KIF.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/KIF-Quick/KIF_Quick.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Quick/Quick.framework"
-fi
-if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/KIF/KIF.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/KIF-Quick/KIF_Quick.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Quick/Quick.framework"
-fi
