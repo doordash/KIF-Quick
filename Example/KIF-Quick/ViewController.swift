4453e2aae49821717325873a55e7f5b8f6cd73db
Paul Zabelin <pzabelin+git@gmail.com>
2016-12-25 18:46:47 -0800

    refactor on tap handler

--- Example/KIF-Quick/ViewController.swift
+++ Example/KIF-Quick/ViewController.swift
@@ -1,9 +1,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBAction func onTap(sender: AnyObject) {
-        if let button = sender as? UIButton {
-            button.setTitle("Thank you!", forState: .Normal)
-        }
+    @IBAction func onTap(button: UIButton) {
+        button.setTitle("Thank you!", forState: .Normal)
     }
 }
